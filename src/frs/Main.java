d1ae91d99332ba550439bfff7309d58d9dac8517
aditya <sam.aditya297@gmail.com>
2021-01-19 01:08:35 +0530

    checkpoint5

--- src/frs/Main.java
+++ src/frs/Main.java
@@ -5,32 +5,41 @@ public class Main {
     public static void main(String[] args) {
 
         Passenger p = new Passenger();
-        System.out.println(p.getPassengerDetails());
+        /*System.out.println(p.getPassengerDetails());
 
-        System.out.println();
+        System.out.println();*/
 
         Flight f = new Flight();
         f.flightDetails("6E 444","Indigo");
-        System.out.println(f.checkSeatAvailability());
+        /*System.out.println(f.checkSeatAvailability());
 
-        System.out.println();
+        System.out.println();*/
 
         RegularTicket rT = new RegularTicket("ABC123","Delhi","Bangalore","12:45 PM",
                 "2:30 PM",3500,false, f, p, "veg meal");
-        if(rT.isCancelled()){
+        /*if(rT.isCancelled()){
             System.out.println("flight is cancelled!");
         }else{
             System.out.println(rT.getRegularTicketDetails());
-        }
+        }*/
 
         System.out.println();
 
         TouristTicket tT = new TouristTicket("XYZ321","Delhi","Bangalore","12:45 PM",
                 "2:30 PM",7000,false, f, p,"GreenLeaf, bangalore", new String[]{"A", "B", "C", "D"});
-        if(tT.isCancelled()){
+        /*if(tT.isCancelled()){
             System.out.println("flight is cancelled!");
         }else{
             System.out.println(tT.getTouristTicketDetails());
-        }
+        }*/
+        printTicketDetails(rT);
+        printTicketDetails(tT);
+
+    }
+    public static void printTicketDetails(RegularTicket regularTicket){
+        System.out.println(regularTicket.pnr);
+    }
+    public static void printTicketDetails(TouristTicket touristTicket){
+        System.out.println(touristTicket.pnr);
     }
 }
